96d3a9c82891a38381f17126e3133718ffa58276
JasonMcKinney <jmckandoidman2@gmail.com>
2020-05-14 23:08:38 -0500

    Created UI (Activity / Adapter)

---
+++ app/src/main/java/com/example/headspacecodechallenge/ui/adapter/ImageViewHolder.kt
@@ -0,0 +1,29 @@
+package com.example.headspacecodechallenge.ui.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.headspacecodechallenge.db.entites.ImageEntry
+import kotlinx.android.synthetic.main.item_photo.view.*
+
+class ImageViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+    fun bindItem(image: ImageEntry) {
+        //populate text
+        itemView.imageAuthor.text = image.author
+        val dimensionText = image.height.toString() + " x " + image.width.toString()
+        itemView.imageDimensions.text = dimensionText
+        // load image
+        Glide.with(itemView).load(image.download_url).into(itemView.image)
+        itemView.setOnClickListener {
+            if (itemView.imageAuthor.visibility == View.GONE) {
+                itemView.imageAuthorTitle.visibility = View.VISIBLE
+                itemView.imageAuthor.visibility = View.VISIBLE
+                itemView.imageDimensions.visibility = View.VISIBLE
+            } else {
+                itemView.imageAuthorTitle.visibility = View.GONE
+                itemView.imageAuthor.visibility = View.GONE
+                itemView.imageDimensions.visibility = View.GONE
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/headspacecodechallenge/ui/main/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.headspacecodechallenge.ui.main
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.headspacecodechallenge.R
+import com.example.headspacecodechallenge.db.ImageDatabase
+import com.example.headspacecodechallenge.db.entites.ImageEntry
+import com.example.headspacecodechallenge.network.WebService
+import com.example.headspacecodechallenge.repository.ImageRepositoryImpl
+import com.example.headspacecodechallenge.ui.adapter.ImageAdapter
+import com.example.headspacecodechallenge.viewmodel.MainViewModel
+import com.example.headspacecodechallenge.viewmodel.factory.MainViewModelFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+    lateinit var imageAdapter: ImageAdapter
+    private var imageDatabase: ImageDatabase? = null
+    private var pageCounter = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initRecycerView()
+
+        createDBInstance()
+
+        viewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(ImageRepositoryImpl(WebService.instance, imageDatabase))
+        ).get(MainViewModel::class.java)
+
+        viewModel.state.observe(this, Observer { appState ->
+            when (appState) {
+                is MainViewModel.AppState.SUCCESS -> displayImages(appState.imageList)
+                is MainViewModel.AppState.ERROR -> displayMessage(appState.message)
+                is MainViewModel.AppState.EMPTY -> displayEmpty()
+                is MainViewModel.AppState.LOADING -> displayLoading()
+            }
+        })
+
+        if (!viewModel.loaded) {
+            viewModel.getImages(pageCounter)
+        }
+    }
+
+    private fun displayLoading() {
+        progressBar.visibility = View.VISIBLE
+        rvImages.visibility = View.GONE
+        containerMessage.visibility = View.GONE
+    }
+
+    private fun displayEmpty() {
+        imageAdapter.images.clear()
+        imageAdapter.notifyDataSetChanged()
+
+        progressBar.visibility = View.GONE
+        rvImages.visibility = View.VISIBLE
+        containerMessage.visibility = View.GONE
+
+        if (imageAdapter.itemCount > 0) {
+            Toast.makeText(this, "No New Items Were Added", Toast.LENGTH_LONG)
+                .show()
+        } else {
+            Toast.makeText(this, "There are no New Items to add", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    private fun displayMessage(message: String) {
+
+        progressBar.visibility = View.GONE
+        rvImages.visibility = View.GONE
+        containerMessage.visibility = View.VISIBLE
+
+        messageText.text = message
+    }
+
+    private fun displayImages(images: List<ImageEntry>) {
+
+        imageAdapter.images.clear()
+        imageAdapter.images.addAll(images)
+        imageAdapter.notifyDataSetChanged()
+
+        progressBar.visibility = View.GONE
+        rvImages.visibility = View.VISIBLE
+        containerMessage.visibility = View.GONE
+
+        pageCounter++
+    }
+
+    private fun createDBInstance() {
+        imageDatabase = ImageDatabase.getInstance(application)
+    }
+
+    private fun initRecycerView() {
+        rvImages.layoutManager = LinearLayoutManager(this)
+        imageAdapter = ImageAdapter(mutableListOf())
+        rvImages.adapter = imageAdapter
+    }
+}
